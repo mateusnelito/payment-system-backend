12c7d7bff25f2b59ce0ac900cf1279392ba77bd2
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-03 13:59:37 +0100

    feat: create clientError class file

---
+++ src/utils/client-error.util.ts
@@ -0,0 +1,17 @@
+import HttpStatusCodes from './http-status-codes.util';
+
+interface errors {
+  [key: string]: string[];
+}
+
+export default class ClientError extends Error {
+  public readonly statusCode: number;
+  public readonly errors: errors;
+
+  constructor(message: string, errors: errors, statusCode?: number) {
+    super(message);
+    this.name = 'ClientError';
+    this.errors = errors;
+    this.statusCode = statusCode ?? HttpStatusCodes.BAD_REQUEST;
+  }
+}
