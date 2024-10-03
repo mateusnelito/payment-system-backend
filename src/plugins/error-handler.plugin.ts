deee7753d6ea932a88984dacfc80029385bdef7e
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-03 10:28:19 +0100

    feat: add ZodError handling

--- src/plugins/error-handler.plugin.ts
+++ src/plugins/error-handler.plugin.ts
@@ -1,9 +1,19 @@
 import { FastifyInstance } from 'fastify';
 import fastifyPlugin from 'fastify-plugin';
 import HttpStatusCodes from '../utils/http-status-codes.util';
+import { ZodError } from 'zod';
+import formatZodErrors from '../utils/format-zod-errors.util';
 
 export default fastifyPlugin(async (server: FastifyInstance) => {
   server.setErrorHandler((err, _, reply) => {
+    // Validation errors
+    if (err instanceof ZodError) {
+      return reply.status(HttpStatusCodes.BAD_REQUEST).send({
+        message: 'Input validation error',
+        errors: formatZodErrors(err),
+      });
+    }
+
     console.error(err);
 
     return reply.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send({
