cbef6b8fa7786a09b9d7c6dbf2accd1f723beb73
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-03 09:38:17 +0100

    feat: create user schema file

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+import {
+  BIRegex,
+  fullNameRegex,
+  strongPasswordRegex,
+} from '../utils/regex.util';
+
+const userSchema = z.object({
+  id: z.string(),
+  fullName: z
+    .string()
+    .trim()
+    .min(2)
+    .max(100)
+    .regex(fullNameRegex, 'invalid full name'),
+  BI: z.string().trim().regex(BIRegex, 'Invalid BI number'),
+  email: z.string().trim().email(),
+  password: z
+    .string()
+    .trim()
+    .regex(
+      strongPasswordRegex,
+      'password must be strong and between 6 and 255 characters long'
+    ),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+});
+
+export const createUserSchema = userSchema.omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true,
+});
