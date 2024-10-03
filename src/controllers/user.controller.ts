cc6dcc2bb299ffc929bde64e8ac353ee148c8f3a
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-03 20:00:53 +0100

    refactor: refactor create user controller to reflect create user schema

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { createUserSchema } from '../schemas/user.schema';
-import {
-  checkUserExistence,
-  checkUserTypeExistence,
-  createUser,
-} from '../services/user.service';
+import { checkAccountTypeExistence } from '../services/account.service';
+import { checkUserExistence, createUser } from '../services/user.service';
 import HttpStatusCodes from '../utils/http-status-codes.util';
 
 export async function createUserController(
@@ -12,11 +9,15 @@ export async function createUserController(
   reply: FastifyReply
 ) {
   const data = createUserSchema.parse(request.body);
-  const { email, BI, userTypeId } = data;
+  const {
+    email,
+    BI,
+    account: { accountTypeId },
+  } = data;
 
   await Promise.all([
     checkUserExistence(email, BI),
-    checkUserTypeExistence(userTypeId),
+    checkAccountTypeExistence(accountTypeId),
   ]);
 
   return reply.status(HttpStatusCodes.CREATED).send(await createUser(data));
