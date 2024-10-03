d3a22a538eb4f2a5579db20eb9b8f29b9dba4e48
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-03 13:59:55 +0100

    feat: add clientError handling and remove errors from internal server error handling

--- src/plugins/error-handler.plugin.ts
+++ src/plugins/error-handler.plugin.ts
@@ -3,6 +3,7 @@ import fastifyPlugin from 'fastify-plugin';
 import HttpStatusCodes from '../utils/http-status-codes.util';
 import { ZodError } from 'zod';
 import formatZodErrors from '../utils/format-zod-errors.util';
+import ClientError from '../utils/client-error.util';
 
 export default fastifyPlugin(async (server: FastifyInstance) => {
   server.setErrorHandler((err, _, reply) => {
@@ -14,12 +15,17 @@ export default fastifyPlugin(async (server: FastifyInstance) => {
       });
     }
 
+    // Client Errors
+    if (err instanceof ClientError) {
+      const { statusCode, message, errors } = err;
+      return reply.status(statusCode).send({ message, errors });
+    }
+
     console.error(err);
 
     return reply.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send({
       statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
       message: 'Internal server error!',
-      errors: err,
     });
   });
 });
