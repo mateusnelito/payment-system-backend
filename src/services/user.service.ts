dd5122b6ef7a0c67d73348a06e23bbe0669d2140
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-03 19:53:05 +0100

    refactor: refactor check user existence and create user services

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,6 +2,7 @@ import { prisma } from '../lib/prisma.lib';
 import { createUserDataType } from '../schemas/user.schema';
 import { hashPassword } from '../utils/bcrypt.util';
 import ClientError from '../utils/client-error.util';
+import HttpStatusCodes from '../utils/http-status-codes.util';
 import { generateNanoId } from '../utils/nanoid.util';
 
 export async function checkUserExistence(email: string, BI: string) {
@@ -10,48 +11,67 @@ export async function checkUserExistence(email: string, BI: string) {
     select: { email: true, BI: true },
   });
 
-  if (user) {
-    if (user.email === email)
-      throw new ClientError('invalid email', {
+  if (user?.email === email)
+    throw new ClientError(
+      'invalid email',
+      {
         email: ['already in use'],
-      });
+      },
+      HttpStatusCodes.CONFLICT
+    );
 
-    if (user.BI === BI)
-      throw new ClientError('invalid BI', {
+  if (user?.BI === BI)
+    throw new ClientError(
+      'invalid BI',
+      {
         BI: ['already in use'],
-      });
-  }
-}
-
-export async function checkUserTypeExistence(id: number) {
-  const userType = await prisma.userType.findUnique({
-    where: { id },
-    select: { id: true },
-  });
-
-  if (!userType)
-    throw new ClientError('invalid userTypeId', {
-      userTypeId: ["don't exists"],
-    });
+      },
+      HttpStatusCodes.CONFLICT
+    );
 }
 
 export async function createUser(data: createUserDataType) {
-  const { fullName, BI, email, password, userTypeId } = data;
+  const {
+    fullName,
+    BI,
+    email,
+    password,
+    account: { accountTypeId, initialBalance },
+  } = data;
+
+  const [userId, passwordHash, accountId] = await Promise.all([
+    generateNanoId(),
+    hashPassword(password),
+    generateNanoId(),
+  ]);
 
   const user = await prisma.user.create({
     data: {
-      id: await generateNanoId(),
+      id: userId,
       fullName,
       BI,
       email,
-      passwordHash: await hashPassword(password),
-      userTypeId,
+      passwordHash,
+      Account: {
+        create: {
+          id: accountId,
+          accountTypeId,
+          balance: initialBalance,
+        },
+      },
     },
     include: {
-      userType: {
+      Account: {
         select: {
           id: true,
-          name: true,
+          balance: true,
+          createdAt: true,
+          AccountType: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
         },
       },
     },
@@ -62,9 +82,14 @@ export async function createUser(data: createUserDataType) {
     fullName: user.fullName,
     BI: user.BI,
     email: user.email,
-    userType: {
-      id: user.userType.id,
-      name: user.userType.name,
+    account: {
+      id: user.Account[0].id,
+      balance: user.Account[0].balance,
+      accountType: {
+        id: user.Account[0].AccountType.id,
+        name: user.Account[0].AccountType.name,
+      },
     },
+    createdAt: user.createdAt,
   };
 }
