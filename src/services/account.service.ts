1b5c3f6e8b608c66fcc1598b7e3cf4059303a3a8
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-04 07:50:12 +0100

    feat: add service to create account

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -1,7 +1,9 @@
 import { AccountType } from '../constants/account.type';
 import { prisma } from '../lib/prisma.lib';
+import { createUserAccountDataType } from '../schemas/user.schema';
 import ClientError from '../utils/client-error.util';
 import HttpStatusCodes from '../utils/http-status-codes.util';
+import { generateNanoId } from '../utils/nanoid.util';
 
 export async function checkUserAccountTypeExistence(
   userId: string,
@@ -18,4 +20,35 @@ export async function checkUserAccountTypeExistence(
     throw new ClientError('invalid account type', HttpStatusCodes.CONFLICT, {
       type: ['The user already has an account of this type'],
     });
+}
+
+export async function createAccount(
+  userId: string,
+  data: createUserAccountDataType
+) {
+  const { type: accountType, initialBalance } = data;
+  const accountId = await generateNanoId();
+
+  const account = await prisma.account.create({
+    data: {
+      id: accountId,
+      userId,
+      accountTypeId: AccountType[accountType],
+      balance: initialBalance,
+    },
+    include: {
+      AccountType: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
+
+  return {
+    id: account.id,
+    type: account.AccountType.name,
+    balance: account.balance,
+    createdAt: account.createdAt,
+  };
 }
