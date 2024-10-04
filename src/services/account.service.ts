70c58b2af46302f94b440d7c127a1f889ce4630c
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-04 07:27:54 +0100

    feat: add check user account type existence service

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -1,1 +1,21 @@
+import { AccountType } from '../constants/account.type';
+import { prisma } from '../lib/prisma.lib';
+import ClientError from '../utils/client-error.util';
+import HttpStatusCodes from '../utils/http-status-codes.util';
 
+export async function checkUserAccountTypeExistence(
+  userId: string,
+  accountType: keyof typeof AccountType
+) {
+  const account = await prisma.account.findUnique({
+    where: {
+      userId_accountTypeId: { userId, accountTypeId: AccountType[accountType] },
+    },
+    select: { id: true },
+  });
+
+  if (account)
+    throw new ClientError('invalid account type', HttpStatusCodes.CONFLICT, {
+      type: ['The user already has an account of this type'],
+    });
+}
