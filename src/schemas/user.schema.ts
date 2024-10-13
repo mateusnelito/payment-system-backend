515f4c5933bb3f836aeb90e687eb5fc440c411f9
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-13 22:24:52 +0100

    feat: add nanoid type for id and improve create user schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -8,7 +8,7 @@ import { accountSchema } from './account.schema';
 import { clientErrorSchema } from './error.schema';
 
 const userSchema = z.object({
-  id: z.string().trim(),
+  id: z.string().trim().nanoid(),
   fullName: z
     .string()
     .trim()
@@ -24,17 +24,13 @@ const userSchema = z.object({
       strongPasswordRegex,
       'password must be strong and between 6 and 255 characters long'
     ),
-  account: accountSchema.omit({
-    id: true,
-    userId: true,
-    createdAt: true,
-  }),
+  account: accountSchema.pick({type: true, initialBalance: true}),
   createdAt: z.date(),
   updatedAt: z.date(),
 });
 
 export const createUserSchema = {
-  summary: 'Create a new user',
+  summary: 'Creates a new user with the provided details.',
   tags: ['users'],
   body: userSchema.omit({
     id: true,
@@ -45,8 +41,7 @@ export const createUserSchema = {
     201: z.object({
       status: z.string().default('success'),
       data: userSchema.omit({ password: true }).extend({
-        account: accountSchema
-          .omit({ userId: true, createdAt: true, initialBalance: true })
+        account: accountSchema.pick({id: true, type: true})
           .extend({ balance: z.number().int() }),
       }),
     }),
