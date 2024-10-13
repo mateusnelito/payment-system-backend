7bce8e050ec74da4fab9aa687e3643cee6a2e53e
Mateus Vale Celestino Nelito <mateusvalecelestinod@gmail.com>
2024-10-13 16:06:06 +0100

    refactor: remove response error check

--- src/plugins/error-handler.plugin.ts
+++ src/plugins/error-handler.plugin.ts
@@ -1,23 +1,10 @@
-import { FastifyInstance, FastifyReply } from 'fastify';
+import { FastifyInstance } from 'fastify';
 import fastifyPlugin from 'fastify-plugin';
-import {
-  hasZodFastifySchemaValidationErrors,
-  isResponseSerializationError,
-} from 'fastify-type-provider-zod';
+import { hasZodFastifySchemaValidationErrors } from 'fastify-type-provider-zod';
 import { ResponseStatus } from '../constants/response-status.type';
 import ClientError from '../utils/client-error.util';
 import HttpStatusCodes from '../utils/http-status-codes.util';
 import { formatZodValidationErrors } from '../utils/format-zod-validation-errors.util';
-
-function sendServerError(reply: FastifyReply) {
-  return reply.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send({
-    status: ResponseStatus.FAIL,
-    data: {
-      message: 'Internal server error!',
-      code: HttpStatusCodes.INTERNAL_SERVER_ERROR,
-    },
-  });
-}
 
 export default fastifyPlugin(async (server: FastifyInstance) => {
   server.setErrorHandler((err, req, reply) => {
@@ -36,11 +23,6 @@ export default fastifyPlugin(async (server: FastifyInstance) => {
       });
     }
 
-    if (isResponseSerializationError(err)) {
-      console.error(`Error Serializing ${err.validationContext} \n: ${err}`);
-      sendServerError(reply);
-    }
-
     // Client Errors
     if (err instanceof ClientError) {
       const { statusCode: code, message, errors } = err;
@@ -50,6 +32,12 @@ export default fastifyPlugin(async (server: FastifyInstance) => {
     }
 
     console.error(err);
-    sendServerError(reply);
+    return reply.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send({
+      status: ResponseStatus.FAIL,
+      data: {
+        message: 'Internal server error!',
+        code: HttpStatusCodes.INTERNAL_SERVER_ERROR,
+      },
+    });
   });
 });
